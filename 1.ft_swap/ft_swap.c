f8b32f0931493423d37b574eff71dbd33b7b6d3b
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-20 12:55:09 +0700

    ft-swap

---
+++ 1.ft_swap/ft_swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void	ft_swap(int *a, int *b){
+    int c;
+    c = *a;
+    *a = *b;
+    *b = c;
+}
+
+int main(int argc, const char** argv) {
+    int a = 1;
+    int b = 2;
+    printf("%d",a);
+    printf("%d",b);
+    ft_swap(a,b);
+    printf("%d",a);
+    printf("%d",b);
+    return 0;
+}
