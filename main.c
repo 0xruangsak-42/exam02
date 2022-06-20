0ad1ad36ac87831006bd0eaf4d923fd5a8cc9bdb
0xRuangsak <astin@Astins-MacBook.local>
2022-06-20 14:10:51 +0700

    add main

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 int main(int argc, const char** argv) {
     printf("%d",argc);
     printf("\n");
-    printf("%s",argv);
+    printf("%s",argv[0]);
+    printf("\n");
+    printf("%s",argv[1]);
+    printf("\n");
+    printf("%s",argv[2]);
     return 0;
 }
