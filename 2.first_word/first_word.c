797b86717f88862e9df276a68411b0ba8f1c1f1f
0xRuangsak <astin@Astins-MacBook.local>
2022-06-20 14:06:20 +0700

    tuning

--- 2.first_word/first_word.c
+++ 2.first_word/first_word.c
@@ -3,10 +3,11 @@
 
 int main(int argc, const char** argv) {
     int i = 0;
-    while (argv[1] != " " || argv[1] != '\t')
+    while (argv[1][i] != ' ' || argv[1][i] != '\t')
     {
         printf("%c",argv[1][i]);
         i += 1;
     }
+    printf("\n");
     return 0;
 }
