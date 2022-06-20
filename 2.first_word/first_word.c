8a175df45c48e8d47c2c11a1b28e5b4605600942
0xRuangsak <astin@Astins-MacBook.local>
2022-06-20 14:20:46 +0700

    fixed bug

--- 2.first_word/first_word.c
+++ 2.first_word/first_word.c
@@ -3,9 +3,9 @@
 
 int main(int argc, const char** argv) {
     int i = 0;
-    while (argv[1][i] != ' ' || argv[1][i] != '\t')
+    while (argv[1][i] != ' ' && argv[1][i] != '\t')
     {
-        printf("%c",argv[1][i]);
+        write(1,&argv[1][i],1);
         i += 1;
     }
     printf("\n");
