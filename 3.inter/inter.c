729c9d2833d8785ec9c7d106bb062fd655d6c929
0xRuangsak <astin@Astins-MacBook.local>
2022-06-21 15:43:40 +0700

    inter

--- 3.inter/inter.c
+++ 3.inter/inter.c
@@ -3,24 +3,28 @@
 void ft_union(char *str1, char *str2)
 {
     int i = 0;
-    int j = 0;
-    while (str1[i++] != '\0')
+    while (str1[i] != '\0')
     {
-        while (str2[j++] != '\0')
+        int j = 0;
+        while (str2[j] != '\0')
         {
             if (str1[i] == str2[j])
             {
-                printf("%c",str1[i]);
+                printf("%c", str1[i]);
+                break;
             }
+            j += 1;
         }
+        i += 1;
     }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     if (argc == 3)
     {
         ft_union(argv[1], argv[2]);
     }
-    printf('\n');
+    printf("\n");
     return 0;
 }
