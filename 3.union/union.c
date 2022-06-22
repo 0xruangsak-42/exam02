66e16b96a06bbbbbbaffc0a11af7e09fcb4846e2
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-22 11:24:41 +0700

    union

--- 3.union/union.c
+++ 3.union/union.c
@@ -14,5 +14,43 @@ int check_doubles(char *str, char c){
 
 int check_doubles_pos(char *str, char c, int pos){
     int i = 0;
-    
+    while (i++ < pos)
+    {
+        if (str[i] == c)
+        {
+            return(0);
+        }
+    }
+    return(1);
+}
+
+void ft_union(char *str1, char *str2){
+    int i = 0;
+    while (str1[i++] != '\0')
+    {
+        if (check_doubles_pos(str1,str1[i],i) == 1)
+        {
+            write(1,&str1[i],1);
+        }
+    }
+    i = 0;
+    while (str2[i++] != '\0')
+    {
+        if (check_doubles(str1 ,str2[i]) == 1)
+        {
+            if (check_doubles_pos(srt2, str2[i],i) == 1)
+            {
+                write(1,&str2[i],1);
+            }
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    if (ac == 3)
+    {
+        ft_union(av[1],av[2]);
+    }
+    write(1,"\n",1);
+    return 0;
 }
