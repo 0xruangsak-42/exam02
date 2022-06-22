b65c67cf7ccd41fb1d6446f79b01128079650c2e
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-22 15:09:17 +0700

    add from another hithuv I don't understand

--- 6.ft_itoa/ft_itoa.c
+++ 6.ft_itoa/ft_itoa.c
@@ -1,8 +1,52 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char** argv) {
-    int i = atoi(argv[1]);
-    printf("%d \n",itoa(i));
-    return 0;
+int	len(long nb)
+{
+	int	len = 0;
+	if (nb < 0)
+	{
+		nb *= -1;
+		len++;
+	}
+	while (nb > 0)
+	{
+		nb /= 10;
+		len++;
+	}
+	return(len);
+}
+char	*ft_itoa(int nb)
+{
+	char	*str;
+	long	n;
+	int	i;
+
+	n = nb;
+	i = len(n);
+	if(!(str = (char *)malloc(i + 1)))
+		return(0);
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = 48;
+		return(str);
+	}
+	if(n < 0)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		str[i] = 48 + (n % 10);
+		n /= 10;
+		i--;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_itoa(1342345));
 }
