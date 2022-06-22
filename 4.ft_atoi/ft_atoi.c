61078d4f098e1744f79d15b427e53ec9221d55db
0xRuangsak <astin@Astins-MacBook.local>
2022-06-22 14:08:36 +0700

    main

--- 4.ft_atoi/ft_atoi.c
+++ 4.ft_atoi/ft_atoi.c
@@ -1,15 +1,25 @@
-int ft_atoi(char *str){
+#include <stdio.h>
+int ft_atoi(const char *str)
+{
     int i = 0;
     int sign = 1;
     int out = 0;
-    if (str[i++] == '-')
+    if (str[i] == '-')
     {
-        sign = -1
+        sign = -1;
+        i += 1;
     }
-    while (str[i++] != '\0')
+    while (str[i] != '\0')
     {
         out *= 10;
         out += str[i] - '0';
+        i += 1;
     }
-    return (sign*out)
+    return (sign * out);
+}
+
+int main(int argc, char **argv)
+{
+    ft_atoi(argv[1]);
+    return 0;
 }
